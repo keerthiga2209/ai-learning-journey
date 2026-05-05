81f0e889365b1d4100547800919a4a85a53487dc
keerthiga2209 <69038006+keerthiga2209@users.noreply.github.com>
2026-05-05 23:28:49 +0530

    Create functions.py

---
+++ day-01-python-basics/functions.py
@@ -0,0 +1,16 @@
+#even or odd number using function
+def even_odd_fun(num):
+  if(num%2==0):
+    print("even")
+  else:
+    print("odd")
+
+even_odd_fun(5)
+
+#sum of 1 to n using function
+def sum_1_to_n(n):
+  for i in range(1,n+1):
+    total=total+i
+  print(total)
+
+#largest of three numbers
